81c35bc0df92674cdad3f2402b7a1b94302075af
Piya Yim <piya.yim2003@gmail.com>
2023-01-02 14:58:11 +0700

    add menu cloud

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+function displayCloudMenu(){
+    document.querySelector("#burger-img").style.display = 'none';
+    document.querySelector("#burger-menu").style.display = 'block';
+    document.querySelector("#cloud-cross-img").style.display = 'block';
+}
+function closeCloudMenu(){
+    document.querySelector("#burger-img").style.display = 'block';
+    document.querySelector("#burger-menu").style.display = 'none';
+    document.querySelector("#cloud-cross-img").style.display = 'none';
+}
