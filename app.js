f64c1c4cb9dae95c902d8c0b93d6a034367d741a
Piya Yim <piya.yim2003@gmail.com>
2023-01-03 21:13:12 +0700

    add education info

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 
 
 function displayCloudMenu(){
-    document.querySelector("#burger-img").style.display = 'none';
-    document.querySelector("#burger-menu").style.display = 'block';
-    document.querySelector("#cloud-cross-img").style.display = 'block';
+    document.querySelector("#burger-img").style.display = 'none'
+    document.querySelector("#burger-menu").style.display = 'block'
+    document.querySelector("#cloud-cross-img").style.display = 'block'
 }
 function closeCloudMenu(){
-    document.querySelector("#burger-img").style.display = 'block';
-    document.querySelector("#burger-menu").style.display = 'none';
-    document.querySelector("#cloud-cross-img").style.display = 'none';
+    document.querySelector("#burger-img").style.display = 'block'
+    document.querySelector("#burger-menu").style.display = 'none'
+    document.querySelector("#cloud-cross-img").style.display = 'none'
+}
+
+function displayEducationInfoContainer(){
+    document.querySelector("#education-info-container").style.display = 'block'
+    document.querySelector("#edu-cross-img").style.display = 'block'
+}
+function closeEducationInfoContainer(){
+    document.querySelector("#education-info-container").style.display = 'none'
+    document.querySelector("#edu-cross-img").style.display = 'none'
 }
